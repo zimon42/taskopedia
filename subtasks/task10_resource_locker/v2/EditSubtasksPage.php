ed79ece8f9c4616861529ca1c60d8addd3b24e13
Simon Wrang <simon.wrang2@gmail.com>
2019-03-29 19:37:36 +0100

    ResourceLocker v2 Draglist works. Next getData

--- subtasks/task10_resource_locker/v2/EditSubtasksPage.php
+++ subtasks/task10_resource_locker/v2/EditSubtasksPage.php
@@ -6,18 +6,60 @@ class EditSubtasksPage {
 		$html = "";
 		$html .= <<<HTML
 <h3>Subtasks</h3>
+<div id=subtask_list_div></div>
+<button id=save_subtask_list_button>Save</button>
 HTML;
-
-		$subtasks_arr = JsonFileHandler::readPhpArray("subtasks.txt");
-		for ($i=0; $i<count($subtasks_arr); $i++) {
-			$subtask = $subtasks_arr[$i];
-			$html .= ($i+1) . ". " . $subtask["title"] . "<br>";
-		}
 		return $html;
 	}
 	
 	public function getAddToHead() {
-		return "";
+		$html = "";
+		$html .= <<<HTML
+<link rel="stylesheet" type="text/css" href="drag_list_module/dragable_list.css">
+<script src=drag_list_module/DragableList.js></script>
+<script>
+
+var dragableList = new DragableList();
+
+function addDragableSubtask(subtask) {
+	$("#subtask_list_div").append(
+		"<div class=block style='display:block'>"+ // <-- Changed none to block
+		"    <div style='float:left;width:200px'>"+
+	    "        <img class='cross no_select none_dragable' src=drag_list_module/cross.png width=20 height=20></img>"+
+		"        <span id=vat_display style='position:relative;bottom:3px'>"+subtask.title+"</span>"+
+		"    </div>"+
+		"    <div style='float:left;width:275px;text-align:right'>"+
+		"        <button id=vat_block_edit>Redigera</button>"+
+		"        <button id=vat_block_remove>Ta bort</button>"+
+		"    </div>"+
+	    "</div>"
+	);
+	dragableList.update();
+	
+	// Attach subtask data to block
+	$(".block:last").data(subtask);
+}
+
+$(document).ready(function() {
+HTML;
+		$subtasks_arr = JsonFileHandler::readPhpArray("subtasks.txt");
+		for ($i=0; $i<count($subtasks_arr); $i++) {
+			$subtask = $subtasks_arr[$i];
+			$html .= "addDragableSubtask({title:'" . $subtask["title"] . "'});\n";
+		}
+
+		$html .= <<<HTML
+		
+	$("#save_subtask_list_button").click(function() {
+		alert("save");
+	});		
+		
+});
+
+
+</script>
+HTML;
+		return $html;
 	}
 	
 }
