af293791b1c3c5c5be2c2063a2062849960601bf
Simon Wrang <simon.wrang2@gmail.com>
2019-06-04 17:26:10 +0200

    Fixed updated_result and changed_title events

--- iteration1/SaveResultSubmit.php
+++ iteration1/SaveResultSubmit.php
@@ -3,6 +3,7 @@
 include_once("utils/JsonFileHandler.php");
 include_once("SkeletonPage.php");
 include_once("TaskopediaData.php");
+include_once("TaskNewsData.php");
 
 class SaveResultSubmit extends SkeletonPage {
 	
@@ -16,6 +17,9 @@ class SaveResultSubmit extends SkeletonPage {
 		$taskArr["result"] = $resState;
 		
 		TaskopediaData::setTaskPageData($this->mainTaskId, $taskId, $taskArr);
+		
+		// Register task news event
+		TaskNewsData::addEvent3($this->taskType, $this->mainTaskId, $this->taskId, array("type" => "updated_result"));
 	}
 	
 	public function getAddToHead() {
--- iteration1/SaveTaskinfoSubmit.php
+++ iteration1/SaveTaskinfoSubmit.php
@@ -4,6 +4,7 @@ include_once("utils/JsonFileHandler.php");
 include_once("SkeletonPage.php");
 include_once("TaskopediaData.php");
 include_once("utils/ParamsHandler.php");
+include_once("TaskNewsData.php");
 
 class SaveTaskinfoSubmit extends SkeletonPage {
 	
@@ -14,6 +15,10 @@ class SaveTaskinfoSubmit extends SkeletonPage {
 		
 		$resState = $_POST["res_state"];
 
+		// So we can check if title has changed for news data event, see below
+		$oldTitle = $taskArr["title"];
+		$newTitle = ParamsHandler::getParamValue($resState, "title");
+		
 		$taskArr["title"] = ParamsHandler::getParamValue($resState, "title");
 		$taskArr["description"] = ParamsHandler::getParamValue($resState, "desc");
 		$taskArr["more_info"] = ParamsHandler::getParamValue($resState, "more_info");
@@ -21,6 +26,11 @@ class SaveTaskinfoSubmit extends SkeletonPage {
 		
 		TaskopediaData::setTaskPageData($this->mainTaskId, $taskId, $taskArr);
 
+		// Possible title change news data event
+		if ($oldTitle != $newTitle) {
+			TaskNewsData::addEvent3($this->taskType, $this->mainTaskId, $this->taskId, array("type" => "changed_title"));			
+		}
+		
 		return $resState;
 
 	}
