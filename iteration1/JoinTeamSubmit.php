6d1bf1ae3604859461b147a20e304bca73f3c57b
Simon Wrang <simon.wrang2@gmail.com>
2019-06-20 18:23:44 +0200

    LeaveTaskSubmit button works

--- iteration1/JoinTeamSubmit.php
+++ iteration1/JoinTeamSubmit.php
@@ -3,10 +3,13 @@
 include_once("SkeletonPage.php");
 include_once("login_module/LoginHandler.php");
 include_once("TaskNewsData.php");
+include_once("TaskHandler.php");
 
 class JoinTeamSubmit extends SkeletonPage {
+
+	public $gTaskId; // Set in preHandle and accessed in getAddToHead
 	
-	public function getContent() {
+	public function preHandle() {
 
 		$taskId = TaskopediaData::getTaskPageId($this->taskType, $this->mainTaskId, $this->taskId);
 		$taskArr = TaskopediaData::getTaskPageData($this->mainTaskId, $taskId);
@@ -25,14 +28,35 @@ class JoinTeamSubmit extends SkeletonPage {
 
 		// Register news event
 		TaskNewsData::addEvent3($this->taskType, $this->mainTaskId, $this->taskId, array("type" => "user_joined"));
+	
+		$this->gTaskId = $taskId;
+	}
+	
+	public function getContent() {
 
 		$html = "";
 		$html .= <<<HTML
-You have successfully joined the team<br><br>
-<b>Note</b>: you do not have to be a member of a task team to work with the task page. However being member of a task team lets other users see who are currently working on the task page. Also, if you are a member of a task team, those tasks will show up when you click the "Your page" link at the top of the task page.
+You have successfully joined the team!<br><br>
+<b>Note</b>: you do not have to be a member of a task team to work with the task page. However being member of a task team lets other users see who are currently working on the task page. Also, if you are a member of a task team, those tasks will show up when you click the "Your page" link at the top of the task page.<br><br>
+<button id=go_to_task_button>Go back to task</button>
 HTML;
 		return $html;
 	
+	}
+	
+	public function getAddToHead() {
+		$link = TaskHandler::getLinkToTaskPage($this->mainTaskId, $this->gTaskId);
+		$html = "";
+		$html .= <<<HTML
+<script>
+$(document).ready(function() {
+	$("#go_to_task_button").click(function() {
+		location = "$link";
+	});
+});
+</script>
+HTML;
+		return $html;
 	}
 	
 }
--- iteration1/LeaveTeamSubmit.php
+++ iteration1/LeaveTeamSubmit.php
@@ -3,11 +3,14 @@
 include_once("SkeletonPage.php");
 include_once("login_module/LoginHandler.php");
 include_once("TaskNewsData.php");
+include_once("TaskHandler.php");
 
 class LeaveTeamSubmit extends SkeletonPage {
-	
-	public function getContent() {
-	
+
+	public $gTaskId;
+
+	public function preHandle() {
+		
 		$taskId = TaskopediaData::getTaskPageId($this->taskType, $this->mainTaskId, $this->taskId);
 		$taskArr = TaskopediaData::getTaskPageData($this->mainTaskId, $taskId);
 		
@@ -32,10 +35,33 @@ class LeaveTeamSubmit extends SkeletonPage {
 	
 		// Register news event
 		TaskNewsData::addEvent3($this->taskType, $this->mainTaskId, $this->taskId, array("type" => "user_left"));	
-	
-		return "You have successfully left the team";
-	
+		
+		$this->gTaskId = $taskId;
 	}
+	
+	public function getContent() {
+		$html = "";
+		$html .= <<<HTML
+You have successfully left the team!<br><br>
+<button id=go_to_task_button>Go back to task</button>
+HTML;
+		return $html;
+	}
+	
+	public function getAddToHead() {
+		$link = TaskHandler::getLinkToTaskPage($this->mainTaskId, $this->gTaskId);
+		$html = "";
+		$html .= <<<HTML
+<script>
+$(document).ready(function() {
+	$("#go_to_task_button").click(function() {
+		location = "$link";
+	});
+});
+</script>
+HTML;
+		return $html;
+	}	
 	
 }
 
