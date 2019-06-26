f4cd2ca70be670dc6a9c591ea166d81cf0faf402
Simon Wrang <simon.wrang2@gmail.com>
2019-06-26 21:48:55 +0200

    NewTopicsubmit and NewReplySubmit buttons work

--- iteration1/forum_module/NewReplySubmitPage.php
+++ iteration1/forum_module/NewReplySubmitPage.php
@@ -5,14 +5,17 @@ include_once("utils/GuidCreator.php");
 include_once("DateHandler.php");
 include_once("NewReplyHandler.php");
 include_once("SkeletonPage.php");
+include_once("ForumConfig.php");
 
 class NewReplySubmitPage extends SkeletonPage {
 	
 	public $forumFile;
 	public $topicId;
 	public $content;
+
+	public $isOk;
 	
-	public function getContent() {
+	public function preHandle() {
 		
 		$response = NewReplyHandler::addNewReply(array(
 			"forum_file" => $this->forumFile,
@@ -21,13 +24,39 @@ class NewReplySubmitPage extends SkeletonPage {
 		));
 		if ($response["status"] == "ok") {
 			$html = $response["html"];
-			return "Added reply to topic!<br>$html";
+			// return "Added reply to topic!<br>$html";
+			$this->isOk = true;
 		}		
+		else {
+			$this->isOk = false;
+		}
 		
 	}
 	
+	public function getContent() {
+		$html = "";
+		$html .= <<<HTML
+Your reply was successfully added to the topic!<br><br>
+Click the button below to navigate to the topic page, and then scroll down to the bottom of the page to see your newly created reply<br><br>
+<button id=go_to_topic_button>Go to topic</button>
+HTML;
+		return $html;
+	}
+
 	public function getAddToHead() {
-		return "";
+		$path = ForumConfig::$mainPagePath;
+		$extraParams = ForumConfig::getExtraParams($this);		
+		$html = "";
+		$html .= <<<HTML
+<script>
+$(document).ready(function() {
+	$("#go_to_topic_button").click(function() {
+		location = "$path?page=forum_view_topic&forum_file={$this->forumFile}&topic_id={$this->topicId}&$extraParams";
+	});
+});
+</script>		
+HTML;
+		return $html;
 	}
 	
 }
--- iteration1/forum_module/NewTopicSubmitPage.php
+++ iteration1/forum_module/NewTopicSubmitPage.php
@@ -13,7 +13,10 @@ class NewTopicSubmitPage extends SkeletonPage {
 	public $content;
 	public $sticky;
 	
-	public function getContent() {
+	public $isOk;
+	public $topicId;
+	
+	public function preHandle() {
 		$response = NewTopicHandler::addNewTopic(array(
 			"forum_file" => $this->forumFile,
 			"title" => $this->title,
@@ -31,13 +34,47 @@ class NewTopicSubmitPage extends SkeletonPage {
 				"title" => $this->title
 			));			
 			
-			return "$html<br>New topic has been added";
+			// return "$html<br>New topic has been added";
+			
+			$this->isOk = true;
+			$this->topicId = $response["topic_id"];
+		}
+		else {
+			$this->isOk = false;
 		}
 				
 	}
 	
+	public function getContent() {
+		$html = "";
+		if ($this->isOk) {
+			$html .= <<<HTML
+New topic has been successfully created!<br><br>	
+<button id=go_to_new_topic_button>Go to newly created topic</button>	
+HTML;
+		}
+		else {
+			$html .= <<<HTML
+An error has occurred, this topic was not added			
+HTML;
+		}
+		return $html;
+	}
+	
 	public function getAddToHead() {
-		return "";
+		$path = ForumConfig::$mainPagePath;
+		$extraParams = ForumConfig::getExtraParams($this);		
+		$html = "";
+		$html .= <<<HTML
+<script>
+$(document).ready(function() {
+	$("#go_to_new_topic_button").click(function() {
+		location = "$path?page=forum_view_topic&forum_file={$this->forumFile}&topic_id={$this->topicId}&$extraParams";
+	});
+});
+</script>		
+HTML;
+		return $html;
 	}
 	
 }
